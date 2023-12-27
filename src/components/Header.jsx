6f8caebf177f2cce8d3f4192837dff2f5c35ec54
Kwon Bora <sweetpt88@gmail.com>
2023-12-27 15:30:42 +0900

    style: Header UI setting (#3)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import dlivelogo from "../assets/img/dlivelogo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <FixedSide>
+      <img src={dlivelogo} alt="로고" />
+    </FixedSide>
+  );
 };
 
 export default Header;
+
+const FixedSide = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to bottom, #11998e, #38ef7d);
+  width: 50%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  overflow-y: auto;
+`;
