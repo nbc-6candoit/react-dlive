f0f0b4a2472f2d169284f3ebac60203868105cd2
pompomko <sdw12334@gmail.com>
2023-12-30 08:22:37 +0900

    update프로필 사진 불러옴

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,7 +1,10 @@
 // 마이페이지(Mypage)
 import { useEffect, useState } from "react";
 import { auth } from "../shared/firebase";
-
+import React from "react";
+import styled from "styled-components";
+import defaultphoto from "../assets/img/avatar.png";
+import Avatar from "components/common/Avatar";
 const Mypage = () => {
   const [userProfile, setUserProfile] = useState(null);
 
@@ -40,11 +43,29 @@ const Mypage = () => {
     <div>
       {userProfile && (
         <>
-          <div>제공자 ID: {userProfile.providerId}</div>
           <div>UID: {userProfile.uid}</div>
-          <div>이름: {userProfile.name}</div>
-          <div>이메일: {userProfile.email}</div>
+          <div>닉네임: {userProfile.name}</div>
+          <div>사용자 이메일: {userProfile.email}</div>
           <div>프로필 사진 URL: {userProfile.photoUrl}</div>
+          {/* <div>
+            프로필 사진:
+            <img
+              src={userProfile.photoUrl}
+              alt={defaultphoto}
+              style={{ width: "100px", height: "100px" }}
+            />
+          </div> */}
+          <StLogCard>
+            <StLogWrapper>
+              <div>
+                <Avatar />
+                <h4>{userProfile.name}</h4>
+              </div>
+              <div>
+                <p>{userProfile.email}</p>
+              </div>
+            </StLogWrapper>
+          </StLogCard>
         </>
       )}
     </div>
@@ -52,3 +73,34 @@ const Mypage = () => {
 };
 
 export default Mypage;
+
+const StLogCard = styled.div`
+  width: 80%;
+  max-height: 120px;
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+`;
+
+const StLogWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 0.5rem;
+  overflow: hidden;
+  & div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.5rem;
+  }
+  & p {
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 14px;
+    line-height: 1.6;
+  }
+`;
