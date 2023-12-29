89e96d3c39789327b9fe776b584307a8ccfb70ef
pompomko <sdw12334@gmail.com>
2023-12-29 19:25:50 +0900

    fix mypage

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,43 +1,8 @@
 // 마이페이지(Mypage)
 import React from "react";
-import SpotInfo from "components/SpotDetail/SpotInfo";
-import SpotLog from "components/SpotDetail/SpotLog";
-import styled from "styled-components";
 
-const Spot = () => {
-  return (
-    <>
-      <StImageWrapper>
-        <img
-          src="https://www.kkday.com/ko/blog/wp-content/uploads/chabak_camping_2.jpg"
-          alt="차박명소"
-        />
-      </StImageWrapper>
-      <SpotInfo />
-      <StPageContainer>
-        <SpotLog />
-      </StPageContainer>
-    </>
-  );
+const Mypage = () => {
+  return <div>Mypagessssssss</div>;
 };
 
-export default Spot;
-
-const StPageContainer = styled.div`
-  overflow-y: auto;
-  height: fit-content;
-  margin-bottom: 50px;
-`;
-
-const StImageWrapper = styled.div`
-  height: 350px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  & img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-`;
+export default Mypage;
