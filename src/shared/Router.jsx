f1989eb68be99720077c383ac533efa24b5af0c5
pompomko <sdw12334@gmail.com>
2023-12-30 15:49:40 +0900

    feature/mypage

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddSpot from "../pages/AddSpot";
 import Login from "../pages/Login";
@@ -14,7 +13,6 @@ import NavBar from "../components/NavBar";
 import Log from "../pages/Log";
 import styled from "styled-components";
 import Signup from "../pages/Signup";
-import InfoFix from "../pages/InfoFix";
 
 const Router = () => {
   return (
@@ -24,63 +22,23 @@ const Router = () => {
         <NavBar />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/mypage/:id" element={<Mypage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/spotdetail" element={<SpotDetail />} />
           <Route path="/spot" element={<Spot />} />
           <Route path="/addspot" element={<AddSpot />} />
-          <Route path="/log" element={<Log />} />
+          <Route path="/log/:id" element={<Log />} />
           <Route path="/addlog" element={<AddLog />} />
-          <Route path="/infofix" element={<InfoFix />} />
         </Routes>
         <Footer />
       </StRouterContainer>
     </BrowserRouter>
   );
-=======
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AddSpot from '../pages/AddSpot';
-import Login from '../pages/Login';
-import Main from '../pages/Main';
-import Mypage from '../pages/Mypage';
-import Spot from '../pages/Spot';
-import AddLog from '../pages/AddLog';
-import SpotDetail from '../pages/SpotDetail';
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import NavBar from '../components/NavBar';
-import Log from '../pages/Log';
-import styled from 'styled-components';
-import Signup from '../pages/Signup';
-
-const Router = () => {
-    return (
-        <BrowserRouter>
-            <Header />
-            <StRouterContainer>
-                <NavBar />
-                <Routes>
-                    <Route path='/' element={<Main />} />
-                    <Route path='/mypage/:id' element={<Mypage />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/signup' element={<Signup />} />
-                    <Route path='/spotdetail' element={<SpotDetail />} />
-                    <Route path='/spot' element={<Spot />} />
-                    <Route path='/addspot' element={<AddSpot />} />
-                    <Route path='/log/:id' element={<Log />} />
-                    <Route path='/addlog' element={<AddLog />} />
-                </Routes>
-                <Footer />
-            </StRouterContainer>
-        </BrowserRouter>
-    );
->>>>>>> 2b1af5814159c107364de187fe5e3ec9c33fa42a
 };
 
 export default Router;
 
 const StRouterContainer = styled.div`
-    margin-left: 50%;
+  margin-left: 50%;
 `;
